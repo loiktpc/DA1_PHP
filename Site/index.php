b1c1751caf03bd57dc8127140968fadb4d045a2d
loikt <loiktpc05314@fpt.edu.vn>
2023-11-04 15:17:13 +0700

    [CORE] - Cài đặt src base

---
+++ Site/index.php
@@ -0,0 +1,50 @@
+<?php include "./site/include/header.php" ?>
+<?php
+switch ($_GET['pages']) {
+	case 'site':
+		switch ($_GET['action']) {
+			case 'home':
+				switch ($_GET['layout']) {
+					case 'home':
+						include './Site/src/home/home.php';
+						break;
+					case 'productdetail':
+						include './Site/src/DetailProduct/DetailProduct.php';
+						break;
+					case 'cart':
+						include './Site/src/Cart/cart.php';
+						break;
+					case 'category':
+						include './Site/src/category/category.php';
+						break;
+					case 'login':
+						include './Site/src/auth/LoginUser.php';
+						break;
+					case 'register':
+						include './Site/src/auth/RegisterUser.php';
+						break;
+					case 'forgotpass':
+						include './Site/src/auth/ForgotPass.php';
+						break;
+					case 'checkout':
+						include './Site/src/checkout/CheckOut.php';
+						break;
+					default:
+						include './Site/src/home/home.php';
+						break;
+				}
+				break;
+			default:
+				include './Site/src/home/home.php';
+				break;
+		}
+		break;
+
+
+}
+
+
+?>
+<?php
+include "./site/include/footer.php"
+	?>
