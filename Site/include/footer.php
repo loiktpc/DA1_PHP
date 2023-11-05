f4435d3b55664bc56ae7a5b538cf027ec42a59bf
nhituyethuynh <nhihtpc05227@fpt.edu.vn>
2023-11-05 08:41:18 +0700

    [HTML] - Chỉnh Sửa Footer user

--- Site/include/footer.php
+++ Site/include/footer.php
@@ -4,18 +4,16 @@
         <div class="row">
             <div class="col-lg-3  col-md-6 col-sm-6">
                 <div class="single-footer-widget">
-                    <h6>About Us</h6>
+                    <h6>Về chúng tôi</h6>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore dolore
-                        magna aliqua.
+                        shop được thành lập vào tháng 11/2023
                     </p>
                 </div>
             </div>
             <div class="col-lg-4  col-md-6 col-sm-6">
                 <div class="single-footer-widget">
-                    <h6>Newsletter</h6>
-                    <p>Stay update with our latest</p>
+                    <h6>Liên hệ</h6>
+                    <p>Để lại email nếu bạn gặp vấn đề</p>
                     <div class="" id="mc_embed_signup">
 
                         <form target="_blank" novalidate="true"
@@ -24,8 +22,8 @@
 
                             <div class="d-flex flex-row">
 
-                                <input class="form-control" name="EMAIL" placeholder="Enter Email"
-                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '"
+                                <input class="form-control" name="EMAIL" placeholder="Email"
+                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email '"
                                     required="" type="email">
 
 
@@ -47,43 +45,33 @@
             </div>
             <div class="col-lg-3  col-md-6 col-sm-6">
                 <div class="single-footer-widget mail-chimp">
-                    <h6 class="mb-20">Instragram Feed</h6>
+                    <h6 class="mb-20">Instragram</h6>
                     <ul class="instafeed d-flex flex-wrap">
-                        <li><img src="img/i1.jpg" alt=""></li>
-                        <li><img src="img/i2.jpg" alt=""></li>
-                        <li><img src="img/i3.jpg" alt=""></li>
-                        <li><img src="img/i4.jpg" alt=""></li>
-                        <li><img src="img/i5.jpg" alt=""></li>
-                        <li><img src="img/i6.jpg" alt=""></li>
-                        <li><img src="img/i7.jpg" alt=""></li>
-                        <li><img src="img/i8.jpg" alt=""></li>
+                        <li><img src="./Public/img/aothun/1.jpg" alt="" style="width:70px;height:70px"></li>
+                        <li><img src="./Public/img/aothun/8.jpg" alt="" style="width:70px;height:70px"></li>
+                        <li><img src="./Public/img/aothun/2.jpg" alt="" style="width:70px;height:70px"></li>
+                        <li><img src="./Public/img/aothun/3.jpg" alt="" style="width:70px;height:70px"></li>
+                        <li><img src="./Public/img/aothun/4.jpg" alt="" style="width:70px;height:70px"></li>
+                        <li><img src="./Public/img/aothun/5.jpg" alt="" style="width:70px;height:70px"></li>
+                        <li><img src="./Public/img/aothun/6.jpg" alt="" style="width:70px;height:70px"></li>
+                        <li><img src="./Public/img/aothun/7.jpg" alt="" style="width:70px;height:70px"></li>
                     </ul>
                 </div>
             </div>
             <div class="col-lg-2 col-md-6 col-sm-6">
                 <div class="single-footer-widget">
-                    <h6>Follow Us</h6>
-                    <p>Let us be social</p>
+                    <h6>Theo dõi chúng tôi</h6>
+                    <p>Mạng xã hội của shop</p>
                     <div class="footer-social d-flex align-items-center">
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a>
+                        <a href="https://www.twitter.com"><i class="fa fa-twitter"></i></a>
                         <a href="#"><i class="fa fa-dribbble"></i></a>
-                        <a href="#"><i class="fa fa-behance"></i></a>
+                        <a href="https://www.instagram.com"><i class="fa fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
-            <p class="footer-text m-0">
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;
-                <script>
-                    document.write(new Date().getFullYear());
-                </script> All rights reserved | This template is made with <i class="fa fa-heart-o"
-                    aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-        </div>
+       
     </div>
 </footer>
 <!-- End footer Area -->
