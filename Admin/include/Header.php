087fc26effd0a62ba4da1f0d32cce8f22255dd09
loikt <loiktpc05314@fpt.edu.vn>
2023-11-11 20:56:24 +0700

    [HTML] Trang Thống kê Admin (N3-38)

--- Admin/include/Header.php
+++ Admin/include/Header.php
@@ -9,10 +9,10 @@
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
     <!-- Pignose Calender -->
-    <link href="./Admin/plugins/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet">
+    <!-- <link href="./Admin/plugins/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet"> -->
     <!-- Chartist -->
-    <link rel="stylesheet" href="./Admin/plugins/chartist/css/chartist.min.css">
-    <link rel="stylesheet" href="./Admin/plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css">
+    <!-- <link rel="stylesheet" href="./Admin/plugins/chartist/css/chartist.min.css">
+    <link rel="stylesheet" href="./Admin/plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css"> -->
     <link href="./Admin/plugins/tables/css/datatable/dataTables.bootstrap4.min.css" rel="stylesheet">
     <!-- Custom Stylesheet -->
     <link href="./Admin/css/style.css" rel="stylesheet">
