714895d8b6f6e1598864fccbebbbe8d026acd38a
loikt <loiktpc05314@fpt.edu.vn>
2023-11-11 20:57:34 +0700

    [HTML] trang đăng nhập  admin (N3-40)

--- Admin/auth/LoginAdmin.php
+++ Admin/auth/LoginAdmin.php
@@ -1,1 +1,53 @@
-<h1>login admin </h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NLP Login</title>
+
+    <link href="./Admin/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="login-form-bg h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100">
+                <div class="col-xl-6">
+                    <div class="form-input-content">
+                        <div class="card login-form mb-0">
+                            <div class="card-body pt-5">
+                                <a class="text-center" href="index.html">
+                                    <h4>Login Admin</h4>
+                                </a>
+
+                                <form class="mt-5 mb-5 login-input">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" placeholder="Tài Khoản">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" placeholder="Mật Khẩu">
+                                    </div>
+                                    <button class="btn login-form__btn submit w-100">Đăng Nhập</button>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="./Admin/plugins/common/common.min.js"></script>
+    <script src="./Admin/js/custom.min.js"></script>
+    <script src="./Admin/js/settings.js"></script>
+    <script src="./Admin/js/gleek.js"></script>
+    <script src="./Admin/js/styleSwitcher.js"></script>
+</body>
+
+</html>
