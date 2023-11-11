71a793f17dc56128e6d7197de1225a9cca3cc260
nhituyethuynh <nhihtpc05227@fpt.edu.vn>
2023-11-11 18:58:07 +0700

    [HTML] trang quản lí thể loại sản phẩm admin(N3-44)

--- Admin/src/category/CategoryEdit.php
+++ Admin/src/category/CategoryEdit.php
@@ -1,1 +1,46 @@
-<h1>trang sữa category</h1>
+<div class="content-body" style="min-height: 780px;">
+    <!-- row -->
+
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-validation">
+                            <h3>Cập Nhật Phân Loại</h3>
+                            <form class="form-valide" action="#" method="post" novalidate="novalidate">
+                                <div class="form-group row">
+                                    <label class="col-lg-4 col-form-label" for="val-username">Tên Phân Loại <span
+                                            class="text-danger">*</span>
+                                    </label>
+                                    <div class="col-lg-6">
+                                        <input type="text" class="form-control" id="val-username" name="name"
+                                            placeholder="Áo thun">
+                                    </div>
+                                </div>                                
+                                <div class="form-group row">
+                                    <label class="col-lg-4 col-form-label" for="val-phoneus">Ghi Chú <span
+                                            class="text-danger">*</span>
+                                    </label>
+                                    <div class="col-lg-6">
+                                        <textarea class="form-control" name="" id="" cols="76" rows="3"></textarea>
+                                    </div>
+                                </div>                               
+                                <div class="form-group row">
+                                    <div class="col-lg-8 ml-auto">
+                                        <button type="submit" class="btn btn-primary">Xác Nhận</button>
+                                        <a class="btn btn-outline-info"
+                                            href="/index.php?pages=admin&layout=home&modulde=category&action=list">
+                                            Quay Lại
+                                        </a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- #/ container -->
+</div>
--- Admin/src/category/CategoryList.php
+++ Admin/src/category/CategoryList.php
@@ -1,1 +1,113 @@
-<h1>list caterory admin</h1>
+<div class="content-body" style="min-height: 780px;">
+    <!-- row -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Phân Loại</h4>
+                        <div class="table-responsive">
+                            <div id="DataTables_Table_0_wrapper"
+                                class="dataTables_wrapper container-fluid dt-bootstrap4">
+
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <table class="table table-striped table-bordered zero-configuration dataTable"
+                                            id="DataTables_Table_0" role="grid"
+                                            aria-describedby="DataTables_Table_0_info">
+                                            <thead>
+                                                <tr role="row">
+                                                    <th class="sorting_asc" tabindex="0"
+                                                        aria-controls="DataTables_Table_0" rowspan="1" colspan="1"
+                                                        aria-sort="ascending"
+                                                        aria-label="Name: activate to sort column descending"
+                                                        style="width: 150px;">Tên Phân Loại</th>
+                                                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0"
+                                                        rowspan="1" colspan="1"
+                                                        aria-label="Position: activate to sort column ascending"
+                                                        style="width: 120px;">Ghi Chú</th>
+                                                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0"
+                                                        rowspan="1" colspan="1"
+                                                        aria-label="Salary: activate to sort column ascending"
+                                                        style="width: 64.1042px;">Thao Tác</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr role="row" class="odd">
+                                                    <td class="sorting_1">Áo thun </td>
+                                                    <td>Local Brand
+                                                    </td>                                           
+                                                    <td class="d-flex">
+                                                        <a
+                                                            href="./index.php?pages=admin&layout=home&modulde=category&action=edit">
+                                                            <button style="margin-right: 10px;"
+                                                                class="btn mb-1 btn-flat btn-primary">
+                                                                Sửa
+                                                            </button>
+                                                        </a>
+                                                        <a
+                                                            href="./index.php?pages=admin&layout=home&modulde=category&action=delete">
+                                                            <button class="btn mb-1 btn-flat btn-secondary">
+                                                                Xóa
+                                                            </button>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                            <tfoot>
+                                                <tr>
+                                                    <th rowspan="1" colspan="1">Tên Phân Loại</th>
+                                                    <th rowspan="1" colspan="1">Ghi Chú</th>                                               
+                                                </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-5">
+                                        <div class="dataTables_info" id="DataTables_Table_0_info" role="status"
+                                            aria-live="polite">Còn 4 phân loại</div>
+                                    </div>
+                                    <div class="col-sm-12 col-md-7">
+                                        <div class="dataTables_paginate paging_simple_numbers"
+                                            id="DataTables_Table_0_paginate">
+                                            <ul class="pagination">
+                                                <li class="paginate_button page-item previous disabled"
+                                                    id="DataTables_Table_0_previous"><a href="#"
+                                                        aria-controls="DataTables_Table_0" data-dt-idx="0" tabindex="0"
+                                                        class="page-link">Trước</a></li>
+                                                <li class="paginate_button page-item active"><a href="#"
+                                                        aria-controls="DataTables_Table_0" data-dt-idx="1" tabindex="0"
+                                                        class="page-link">1</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="DataTables_Table_0" data-dt-idx="2" tabindex="0"
+                                                        class="page-link">2</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="DataTables_Table_0" data-dt-idx="3" tabindex="0"
+                                                        class="page-link">3</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="DataTables_Table_0" data-dt-idx="4" tabindex="0"
+                                                        class="page-link">4</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="DataTables_Table_0" data-dt-idx="5" tabindex="0"
+                                                        class="page-link">5</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="DataTables_Table_0" data-dt-idx="6" tabindex="0"
+                                                        class="page-link">6</a></li>
+                                                <li class="paginate_button page-item next" id="DataTables_Table_0_next">
+                                                    <a href="#" aria-controls="DataTables_Table_0" data-dt-idx="7"
+                                                        tabindex="0" class="page-link">Tiếp</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- #/ container -->
+</div>
