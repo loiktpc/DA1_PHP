5ed73c9a694eccaafdf90701a91ed7d631dabd17
phupc06001 <phudmtpc06001@fpt.edu.vn>
2023-11-21 14:47:43 +0700

    [PHP] chức năng đăng ký user  (N3-56)

--- Site/src/auth/RegisterUser.php
+++ Site/src/auth/RegisterUser.php
@@ -1,3 +1,31 @@
+<?php
+if (isset($_POST['register'])) {
+    $UserName = $_POST['username'];
+    $Email = $_POST['email'];
+    $Phone = $_POST['phone'];
+    $PassWord = $_POST['passwords']; 
+    $mahoapass = password_hash($PassWord, PASSWORD_DEFAULT);
+    $user = new User();  
+    $pattern = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$^";
+    $validphone = "/^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/";
+    if (empty($_POST['username']) && empty($_POST['passwords']) && empty($_POST['email']) && empty($_POST['phone'])) {
+        $valid = 'Vui lòng nhập thông tin';       
+    } 
+    else if (strlen($PassWord) <= 6) {
+        $valid_pass = "Mật khẩu phải lớn hơn 6 kí tự";
+    }
+    else if (strlen($UserName) <= 6) {
+        $valid_name = "Tên phải lớn hơn 6 kí tự";
+    } else if (!preg_match($validphone, $Phone)) {
+        $valid_phone = "Số điện thoại không đúng định dạng!";
+    } else if (!preg_match($pattern, $Email)) {
+        $valid_email = "Email không đúng định dạng!";
+    } else {                  
+        $signupuser = $user->Insert_users($UserName, $mahoapass, $Email, $Phone, 1, 2);
+        header("Location: ./index.php?pages=site&action=home&layout=login");
+    }
+}
+?>
 <!-- Start Banner Area -->
 <section class="banner-area organic-breadcrumb">
     <div class="container">
@@ -21,38 +49,47 @@
             <div class="col-lg-6">
                 <div class="login_box_img">
                     <img class="img-fluid" src="./Public/img/aothun/2.jpg" alt="">
-                    <!-- <div class="hover">
-                        <h4>New to our website?</h4>
-                        <p>There are advances being made in science and technology everyday, and a good example of this
-                            is the</p>
-                        <a class="primary-btn" href="registration.html">Create an Account</a>
-                    </div> -->
                 </div>
             </div>
             <div class="col-lg-6">
-                <div class="login_form_inner">
+                <div class="login_form_inner ">
                     <h3> Đăng ký </h3>
-                    <form class="row login_form" action="contact_process.php" method="post" id="contactForm"
-                        novalidate="novalidate">
+                    <form class="row login_form " action="" method="post"  novalidate="novalidate">
                         <div class="col-md-12 form-group">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Tên đăng nhập"
-                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Tên đăng nhập'">
+                            <input type="text" class="form-control"  name="username" placeholder="Tên đăng nhập"  required>
+                            <div class="col-lg-4"></div>
+                            <div class="" style="color: red;">
+                                <?php echo $valid_name ?? "" ?>
+                            </div>
+                        </div><div class="col-md-12 form-group">
+                            <input type="password" class="form-control"  name="passwords" placeholder="Mật khẩu"  required>
+                        <div class="col-lg-4"></div>
+                            <div class="" style="color: red;">
+                                <?php echo $valid_pass ?? "" ?>
+                            </div>
                         </div>
                         <div class="col-md-12 form-group">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Mật khẩu"
-                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Mật khẩu'">
+                            <input type="text" class="form-control"  name="phone" placeholder="Số điện thoại"  required>
+                            <div class="col-lg-4"></div>
+                            <div class="" style="color: red;">
+                                <?php echo $valid_phone ?? "" ?>
+                            </div>
                         </div>
                         <div class="col-md-12 form-group">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Số điện thoại"
-                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Số điện thoại'">
+                            <input type="text" class="form-control"  name="email" placeholder="Địa chỉ Email"  required>
+                            <div class="col-lg-4"></div>
+                            <div class="" style="color: red;">
+                                <?php echo $valid_email ?? "" ?>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-lg-4"></div>
+                            <div class="" style="color: red;">
+                                <?php echo $valid ?? "" ?>
+                            </div>
                         </div>
                         <div class="col-md-12 form-group">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Địa chỉ Email"
-                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Địa chỉ Email'">
-                        </div>
-                        
-                        <div class="col-md-12 form-group">
-                            <button type="submit" value="submit" class="primary-btn">Đăng ký</button>
+                            <button type="submit" name="register" class="primary-btn">Đăng ký</button>
                             <a href="/index.php?pages=site&action=home&layout=login">Bạn đã có tài khoản?</a>
                         </div>
                     </form>
