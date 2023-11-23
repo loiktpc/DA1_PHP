f2698e7e5582a63bd04a335c9ab4190387a0e681
nhituyethuynh <nhihtpc05227@fpt.edu.vn>
2023-11-23 14:56:38 +0700

    [PHP] Đổi mật khẩu khách hàng (N3-82)

--- Site/src/infouser/ChangsPass.php
+++ Site/src/infouser/ChangsPass.php
@@ -1,3 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $user = new User();
+    $userInfo = $user->id($_SESSION['username']);
+    $hash = $user->checkUserpass($_SESSION['username']);
+    if (!empty($_POST['passwords']) && !empty($_POST['newpass']) && !empty($_POST['renewpass'])) {
+        if (strlen($_POST['newpass']) >= 6 && strlen($_POST['newpass']) <=20 && strlen($_POST['renewpass']) >= 6 && strlen($_POST['renewpass']) <= 20) {
+            if (isset($hash[0]) && password_verify($_POST['passwords'], $hash[0]['passwords'])) {
+                if ($_POST['newpass'] == $_POST['renewpass']) {
+                    $password_hash = password_hash($_POST['newpass'], PASSWORD_DEFAULT);
+                    $user->Updateuser($userInfo['id'], $password_hash);
+                    $_SESSION['passwords'] = $_POST['newpass'];
+                    $sucess = "Mật khẩu đã được thay đổi thành công *_*";
+                    header('Location: ./index.php?pages=site&action=home&layout=changpass');
+                    ob_end_flush();
+                } else {
+                    $error = "Mật khẩu mới và xác nhận mật khẩu mới không khớp!";
+                }
+            } else {
+                $error = "Mật khẩu hiện tại không đúng!";
+            }
+        } else {
+            $error = "Mật khẩu mới phải lớn hơn 6 và nhỏ hơn 20 ký tự!";
+        }
+    } else {
+        $error = "Vui lòng nhập dữ liệu!";
+    }
+}
+?>
 <!-- Start Banner Area -->
 <section class="banner-area organic-breadcrumb">
     <div class="container">
@@ -5,8 +35,7 @@
             <div class="col-first">
                 <h1>Tài Khoản</h1>
                 <nav class="d-flex align-items-center">
-                    <a href="/index.php?pages=site&action=home&layout=home">Trang Chủ<span
-                            class="lnr lnr-arrow-right"></span></a>
+                    <a href="/index.php?pages=site&action=home&layout=home">Trang Chủ<span class="lnr lnr-arrow-right"></span></a>
                     <a href="/index.php?pages=site&action=home&layout=changpass">Đổi mật khẩu</a>
                 </nav>
             </div>
@@ -21,29 +50,24 @@
         <div class="row">
             <div class="col-lg-8 posts-list">
                 <div class="single-post row">
-
-
                 </div>
-
-
                 <div class="comment-form">
                     <h4>Đổi Mẩu Khẩu</h4>
-                    <form>
-
+                    <form method="POST" action="">
+                        <p class="text-success"><?php echo $sucess ?? ""; ?></p>
                         <div class="form-group ">
-                            <input type="text" class="form-control" id="subject" placeholder="Mật Khẩu Hiện Tại" />
+                            <input type="password" class="form-control" id="subject" name="passwords" placeholder="Mật Khẩu Hiện Tại" />
                         </div>
                         <div class="form-group ">
 
-                            <input type="text" class="form-control" id="subject" placeholder="Mật khẩu mới" />
+                            <input type="password" class="form-control" id="subject" name="newpass" placeholder="Mật khẩu mới" />
                         </div>
                         <div class="form-group ">
 
-                            <input type="text" class="form-control" id="subject" placeholder="Xác nhận mật khẩu mới" />
+                            <input type="password" class="form-control" id="subject" name="renewpass" placeholder="Xác nhận mật khẩu mới" />
                         </div>
-
-
-                        <button style="border: none;" class="primary-btn submit_btn">Xác Nhận</button>
+                        <p class="text-danger"><?php echo $error ?? ""; ?></p>
+                        <button style="border: none;" type="submit" class="primary-btn submit_btn" name="REQUEST_METHOD">Xác Nhận</button>
                     </form>
                 </div>
             </div>
@@ -58,34 +82,31 @@
 
                         <div class="br"></div>
                     </aside>
-
-
-                    <aside class="single_sidebar_widget post_category_widget">
+                  <aside class="single_sidebar_widget post_category_widget">
                         <h4 class="widget_title">Tài Khoản Của Tôi</h4>
                         <ul class="list cat-list">
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=infouser"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=infouser&id=<?php
+                                                                                                $usernewpass = new User();
+                                                                                                $userInfo = $usernewpass->id($_SESSION['username']);
+                                                                                                echo $userInfo['id']; ?>" class="d-flex justify-content-between">
                                     <p>Hồ sơ</p>
 
                                 </a>
                             </li>
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=changpass"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=changpass" class="d-flex justify-content-between">
                                     <p>đổi mật khẩu</p>
 
                                 </a>
                             </li>
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=favoritecart"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=favoritecart" class="d-flex justify-content-between">
                                     <p>Sản Phẩm Yêu Thích</p>
                                 </a>
                             </li>
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=infocart"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=infocart" class="d-flex justify-content-between">
 
                                     <p>Đơn Hàng</p>
 
