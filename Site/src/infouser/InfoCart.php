73034ee52532c0e693f2bf3810bc85543712c995
nhituyethuynh <nhihtpc05227@fpt.edu.vn>
2023-12-02 14:32:17 +0700

    [PHP]  lưu thông tin lịch sử đơn hàng vào đơn hàng chi tiết(N3-91)

--- Site/src/infouser/InfoCart.php
+++ Site/src/infouser/InfoCart.php
@@ -5,8 +5,7 @@
             <div class="col-first">
                 <h1>Tài Khoản</h1>
                 <nav class="d-flex align-items-center">
-                    <a href="/index.php?pages=site&action=home&layout=home">Trang Chủ<span
-                            class="lnr lnr-arrow-right"></span></a>
+                    <a href="/index.php?pages=site&action=home&layout=home">Trang Chủ<span class="lnr lnr-arrow-right"></span></a>
                     <a href="/index.php?pages=site&action=home&layout=infocart">Thông tin giỏ hàng</a>
                 </nav>
             </div>
@@ -39,28 +38,24 @@
                                 <a href="/index.php?pages=site&action=home&layout=infouser&id=<?php
                                                                                                 $usernewpass = new User();
                                                                                                 $userInfo = $usernewpass->id($_SESSION['username']);
-                                                                                                echo $userInfo['id']; ?>"
-                                    class="d-flex justify-content-between">
+                                                                                                echo $userInfo['id']; ?>" class="d-flex justify-content-between">
                                     <p>Hồ sơ</p>
 
                                 </a>
                             </li>
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=changpass"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=changpass" class="d-flex justify-content-between">
                                     <p>đổi mật khẩu</p>
 
                                 </a>
                             </li>
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=favoritecart"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=favoritecart" class="d-flex justify-content-between">
                                     <p>Sản Phẩm Yêu Thích</p>
                                 </a>
                             </li>
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=infocart"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=infocart" class="d-flex justify-content-between">
 
                                     <p>Đơn Hàng</p>
 
@@ -84,48 +79,66 @@
                             <tr>
                                 <th scope="col">Họ Tên</th>
                                 <th scope="col">Địa Chỉ</th>
-                                <th scope="col">Số Lượng</th>
                                 <th scope="col">Số Điện Thoại</th>
+                                <th scope="col">Số Lượng</th>
                                 <th scope="col">Tổng Tiền</th>
                                 <th scope="col">Trạng Thái</th>
                                 <th scope="col" class="text-center">Thao Tác</th>
                             </tr>
                         </thead>
                         <tbody>
+                            <?php
+                            $order = new Order();
+                            $rows = $order->GetOrder($_SESSION['idLogin']);
+                            foreach ($rows as $list) :
+                                extract($list);
+                            ?>
+                                <tr>
+                                    <td>
+                                        <div class="media">
+                                            <h5> <?php echo $list['nameorder']; ?></h5>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <h5 style="width: 250px;"> <?php echo $list['addressorder']; ?></h5>
+                                    </td>
+                                    <td>
+                                        <h5><?php echo $list['phone']; ?></h5>
 
-                            <tr>
-                                <td>
-                                    <div class="media">
-                                        <h5>Kim Thành Lợi</h5>
-                                    </div>
-                                </td>
-                                <td>
-                                    <h5 style="width: 250px;">44 kenh 30/4 khóm 6 phường 6
-                                        sóc dasdasd assdasdasd</h5>
-                                </td>
-                                <td>
-                                    <h5>123</h5>
-                                </td>
-                                <td>
-                                    <h5>$360.00</h5>
+                                    </td>
+                                    <td>
+                                        <h5><?php
+                                            $order = new Order();
+                                            $totalqty = $order->sumqty($list['id']);
+                                            echo $totalqty['totalqty'];
+                                            ?></h5>
 
-                                </td>
-                                <td>
-                                    <h5>0945202221</h5>
+                                    </td>
+                                    <td>
+                                        <h5><?php echo  $price = number_format($list['total'], 0, '.', '.'); ?>đ</h5>
 
-                                </td>
-                                <td>
-                                    <span class="badge rounded-pill bg-success">Thành Công</span>
-                                </td>
-                                <td class="text-center">
+                                    </td>
+                                    <td>
+                                        <?php
+                                        if ($list['status_order'] == 0) {
+                                            echo '<span class="badge rounded-pill bg-secondary">Chưa xác nhận</span>';
+                                        } else if ($list['status_order'] == 1) {
+                                            echo '<span class="badge rounded-pill bg-primary">Đã xác nhận</span>';
+                                        } else if ($list['status_order'] == 2) {
+                                            echo '<span class="badge rounded-pill bg-warning text-black">Đang giao hàng</span>';
+                                        } else {
+                                            echo '<span class="badge rounded-pill bg-success">Thành công</span>';
+                                        }
+                                        ?>
+                                    </td>
+                                   
+                                        <td class="text-center">
+                                            <a href="/index.php?pages=site&action=home&layout=infocartdetail&id=<?php echo $list['id']; ?>" class="text-black"><button class="genric-btn info" type="submit">Chi tiết</button></a>
+                                        </td>
+                                   
+                                </tr>
 
-                                    <button class="genric-btn info">Chi tiết đơn
-                                        hàng</button>
-
-                                </td>
-                            </tr>
-
-
+                            <?php endforeach; ?>
                         </tbody>
                     </table>
                 </div>
--- Site/src/infouser/InfoCart_detail.php
+++ Site/src/infouser/InfoCart_detail.php
@@ -5,8 +5,7 @@
             <div class="col-first">
                 <h1>Tài Khoản</h1>
                 <nav class="d-flex align-items-center">
-                    <a href="/index.php?pages=site&action=home&layout=home">Trang Chủ<span
-                            class="lnr lnr-arrow-right"></span></a>
+                    <a href="/index.php?pages=site&action=home&layout=home">Trang Chủ<span class="lnr lnr-arrow-right"></span></a>
                     <a href="/index.php?pages=site&action=home&layout=infocartdetail">Thông tin giỏ hàng Chi Tiết</a>
                 </nav>
             </div>
@@ -38,28 +37,24 @@
                                 <a href="/index.php?pages=site&action=home&layout=infouser&id=<?php
                                                                                                 $user = new User();
                                                                                                 $userInfo = $user->id($_SESSION['username']);
-                                                                                                echo $userInfo['id']; ?>"
-                                    class="d-flex justify-content-between">
+                                                                                                echo $userInfo['id']; ?>" class="d-flex justify-content-between">
                                     <p>Hồ sơ</p>
 
                                 </a>
                             </li>
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=changpass"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=changpass" class="d-flex justify-content-between">
                                     <p>đổi mật khẩu</p>
 
                                 </a>
                             </li>
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=favoritecart"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=favoritecart" class="d-flex justify-content-between">
                                     <p>Sản Phẩm Yêu Thích</p>
                                 </a>
                             </li>
                             <li>
-                                <a href="/index.php?pages=site&action=home&layout=infocart"
-                                    class="d-flex justify-content-between">
+                                <a href="/index.php?pages=site&action=home&layout=infocart" class="d-flex justify-content-between">
 
                                     <p>Đơn Hàng</p>
 
@@ -81,29 +76,32 @@
                         </h5>
 
                     </div>
+
+
+
                     <ul class="blog_meta list">
-                        <li><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                                    <path
-                                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
-                                </svg>Kim Thanh Loi</a></li>
-                        <li><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
-                                    <path
-                                        d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />
-                                </svg> 44 kenh 30/4 phuong 6 soc trang sadasdasdasdsadasd </a></li>
-                        <li><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-cash" viewBox="0 0 16 16">
-                                    <path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
-                                    <path
-                                        d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2H3z" />
-                                </svg> 1.000.000đ</a></li>
-                        <li><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
-                                    <path
-                                        d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
-                                    <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                                </svg> 0985632658</a></li>
+                        <?php
+                        $order = new Order();
+                        $listorder = $order->getall($_GET['id']);
+                        foreach ($listorder as $ListOrder) :
+                            extract($ListOrder);
+                            $priceorder = number_format($ListOrder['total'], 0, '.', '.');
+                        ?>
+                            <li><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                                    </svg> <?php echo $ListOrder['name']; ?></a></li>
+                            <li><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                                        <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />
+                                    </svg> <?php echo $ListOrder['address']; ?></a></li>
+                            <li><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash" viewBox="0 0 16 16">
+                                        <path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                                        <path d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2H3z" />
+                                    </svg> <?php echo $priceorder ?>đ</a></li>
+                            <li><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+                                        <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                                        <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                                    </svg> <?php echo $ListOrder['phone']; ?></a></li>
+                        <?php endforeach; ?>
                     </ul>
 
                 </div>
@@ -116,66 +114,61 @@
                     <table class="table">
                         <thead>
                             <tr>
-
-
-                                <th scope="col">Sản Phẩm</th>
-                                <th scope="col">Số lượng</th>
-                                <th scope="col">Mã hóa Đơn</th>
-                                <th scope="col">chuyển Khoản</th>
-                                <th scope="col">giá</th>
-                                <th class="text-center" scope="col">Thao Tác</th>
-
+                                <th scope="col" style="width:300px">Sản Phẩm</th>
+                                <th scope="col" style="width:150px">Số Lượng</th>
+                                <th scope="col" style="width:200px">Mã Hóa Đơn</th>
+                                <th scope="col" style="width:300px">Hình Thức Thanh Toán</th>
+                                <th scope="col" style="width:150px">Giá Tiền</th>
+                                <th class="text-center" scope="col" style="width:150px">Thao Tác</th>
                             </tr>
+
                         </thead>
                         <tbody>
+                            <?php
+                            $order = new Order();
+                            $rows = $order->selectorderdetail($_GET['id']);
+                            foreach ($rows as $list) :
+                                extract($list);
+                                $price = number_format($list['price'], 0, '.', '.');
+                            ?>
+                                <tr>
 
+                                    <td>
+                                        <h5>
+                                            <?php echo $list['nameproduct'] ?>
+                                        </h5>
 
-                            <tr>
+                                    </td>
+                                    <td>
+                                        <h5>
+                                            <?php echo $list['qty_orderdetail']; ?>
+                                        </h5>
+                                    </td>
+                                    <td>
+                                        <h5>
+                                            <?php echo $list['order_code'] ?>
+                                        </h5>
+                                    </td>
+                                    <td>
+                                        <h5>
+                                            <?php echo $list['transfer_money'] == 'cash' ? 'Thanh toán khi nhận hàng' : 'Chuyển khoản'; ?>
 
-                                <td>
-                                    <h5>
-                                        EM ĐỪNG ĐI | SƠN TÙNG M-TP | LOFI CHILL VER. BY ĐỖ HOÀNG NAM
-                                    </h5>
-
-                                </td>
-                                <td>
-                                    <h5>
-                                        568
-                                    </h5>
-                                </td>
-                                <td>
-                                    <h5>
-                                        456789
-                                    </h5>
-                                </td>
-                                <td>
-                                    <h5>
-                                        tiền mặt
-
-
-                                    </h5>
-
-                                </td>
-                                <td>
-                                    <h5>
-                                        1.000.000đ
-                                    </h5>
-
-                                </td>
-                                <td class="text-center">
-
-
-                                    <button type="button" style="width: 110px;" class="genric-btn primary radius">Đánh
-                                        Giá</button>
-
-                                </td>
-                            </tr>
-
-
-
-
-
-
+                                        </h5>
+                                    </td>
+                                    <td>
+                                        <h5>
+                                            <?php echo $price; ?>đ
+                                        </h5>
+                                    </td>
+                                    <td class="text-center">
+                                        <?php if ($list['status_order'] == 3) { ?>
+                                            <a href="./index.php?pages=site&action=home&layout=productdetail&id=<?php echo $list['product_id']; ?>&hasreview=<?php echo $list['user_id']; ?>"><button type="button" style="width: 110px;" class="genric-btn primary radius">Đánh Giá</button></a>
+                                        <?php } else { ?>
+                                            <button type="button" style="width: 110px;" class="btn genric-btn primary radius" disabled>Đánh Giá</button>
+                                        <?php }  ?>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
                         </tbody>
                     </table>
                 </div>
